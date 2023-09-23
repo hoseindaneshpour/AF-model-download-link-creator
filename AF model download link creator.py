7ad69eb9f318e86da5fca26c1362126872488797
hoseindaneshpour <145836511+hoseindaneshpour@users.noreply.github.com>
2023-09-23 13:02:03 +0330

    AF model download link creator.py

--- AF model download link creator.py
+++ AF model download link creator.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# The following code creates guessed download links for AF models corresponding to 85 UP accession codes, which I had dowloaded as a list from the Blast results:
-# TR:A0A448ZPF4
-# TR:B8C250
-# TR:K0RA12
-# ... etc
-# I replaced TR: with empty strings and cut the list at the acc code I had deemed as the last one qualifying when studying the results in the web page then saved as uniprot-euk-acc-list-85.txt which is in the sequences etc folder 
 
 # In[9]:
 
